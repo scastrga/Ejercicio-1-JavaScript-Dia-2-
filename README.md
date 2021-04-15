d162862af76a6b2b37d5a8c2e0f5c3c68d788713
scastrga <scastrga@everis.com>
2021-04-15 11:27:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-vgzrrlejercicio1-blk8i5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-vgzrrlejercicio1-blk8i5)
---
+++ index.js
@@ -0,0 +1,8 @@
+// Crear un array de strings y mostrar por consola el primer, último y penúltimo elementos
+
+var array = ["String 1", "String 2", "String 3", "String 4"];
+console.log(array.length);
+
+console.log(array[0]);
+console.log(array[array.length - 2]); 
+console.log(array[array.length - 1]); 
