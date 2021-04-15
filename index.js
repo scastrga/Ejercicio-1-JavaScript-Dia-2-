0b8fb5d7744500f74550fe18a3354af635c2e124
scastrga <scastrga@everis.com>
2021-04-15 14:19:47 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Crear un array de strings y mostrar por consola el primer, último y penúltimo elementos
 
 var array = ["String 1", "String 2", "String 3", "String 4"];
-console.log(array.length);
+console.log(array);
 
 console.log(array[0]);
 console.log(array[array.length - 2]); 
